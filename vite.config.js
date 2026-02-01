d2e5b9e1a26851387ec37558c2be1a2b33aaf116
Jonveil <rifaat.aslam@gmail.com>
2026-02-01 11:18:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Ini ubat 'global' wajib
+    // Ubat wajib untuk Web3
     "global": "window",
-  },
-  build: {
-    rollupOptions: {
-      // 🔥 INI BAHAGIAN PENTING 🔥
-      // Kita suruh Vercel abaikan library 'Safe' yang bermasalah ni
-      external: [
-        "@safe-window/safe-ethers-adapters",
-        "@safe-window/safe-ethers-adapters/dist/safe-ethers-adapters.esm"
-      ],
-    },
   },
 });
