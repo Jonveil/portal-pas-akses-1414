8114000295d82d77bac7597d12d2dfaa71c1b804
Jonveil <rifaat.aslam@gmail.com>
2026-02-02 05:09:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
+    // Ini 'ubat' untuk pastikan coding blockchain berjalan lancar
     "global": "window",
     "process.env": {},
   },
+  resolve: {
+    alias: {
+      // Kita pastikan dia tak keliru antara folder
+      process: "process/browser",
+    }
+  }
 });
