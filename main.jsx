7e77d1f7c51a888edc4af63e91a0f98a5e0b3b06
Jonveil <rifaat.aslam@gmail.com>
2026-02-01 10:17:32 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-// Pastikan anda ada fail index.css kalau nak style, kalau tak ada boleh padam baris import css
-// import './index.css' 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+
+// Kita bungkus App dengan ThirdwebProvider (Enjin Blockchain)
+// Client ID Tuan: b25286cc43a81f0ecab40b732a0d462c
+// Chain: World Chain
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThirdwebProvider 
+      activeChain="worldchain" 
+      clientId="b25286cc43a81f0ecab40b732a0d462c"
+    >
+      <App />
+    </ThirdwebProvider>
   </React.StrictMode>,
-)
+);
