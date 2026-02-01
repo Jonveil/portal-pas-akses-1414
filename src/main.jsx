f3263552ab1ffcc4aaa9bd0815e4d0e5ec6d8fd5
Jonveil <rifaat.aslam@gmail.com>
2026-02-01 12:09:31 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { WorldChain } from "@thirdweb-dev/chains";
+
+// Client ID Tuan: b25286cc43a81f0ecab40b732a0d462c
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThirdwebProvider 
+      activeChain={WorldChain} 
+      clientId="b25286cc43a81f0ecab40b732a0d462c"
+    >
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>,
+);
