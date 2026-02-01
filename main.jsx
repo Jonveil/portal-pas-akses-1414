4ecd30e8f58dee4868ee94b1d76e8ab4225686c7
Jonveil <rifaat.aslam@gmail.com>
2026-02-01 10:36:02 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -4,9 +4,7 @@ import App from './App';
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { WorldChain } from "@thirdweb-dev/chains";
 
-// Kita bungkus App dengan ThirdwebProvider (Enjin Blockchain)
-// Ini WAJIB supaya Web3Button tak error
-
+// Client ID Tuan: b25286cc43a81f0ecab40b732a0d462c
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThirdwebProvider 
