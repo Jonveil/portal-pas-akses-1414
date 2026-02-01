5505fc8b4df6175d7f8ee2fa9302ba7f20ebd6a7
Jonveil <rifaat.aslam@gmail.com>
2026-02-01 10:25:07 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { WorldChain } from "@thirdweb-dev/chains";
 
 // Kita bungkus App dengan ThirdwebProvider (Enjin Blockchain)
-// Client ID Tuan: b25286cc43a81f0ecab40b732a0d462c
-// Chain: World Chain
+// Ini WAJIB supaya Web3Button tak error
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThirdwebProvider 
-      activeChain="worldchain" 
+      activeChain={WorldChain} 
       clientId="b25286cc43a81f0ecab40b732a0d462c"
     >
       <App />
