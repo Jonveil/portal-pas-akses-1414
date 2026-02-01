1024d763a249bbf823bd6774bf0e0a8eab59a9e1
Jonveil <rifaat.aslam@gmail.com>
2026-02-02 03:36:47 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { WorldChain } from "@thirdweb-dev/chains";
 
-// Client ID Tuan: b25286cc43a81f0ecab40b732a0d462c
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThirdwebProvider 
