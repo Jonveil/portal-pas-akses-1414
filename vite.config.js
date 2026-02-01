ec1680b11b86cd52b4684cc32f94b64269a83020
Jonveil <rifaat.aslam@gmail.com>
2026-02-02 03:09:40 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-
   define: {
-    global: 'window',
+    // Ini sahaja ubat yang kita perlukan untuk Thirdweb
+    "global": "window",
+    "process.env": {},
   },
-
-  resolve: {
-    alias: {
-      '@safe-window/safe-ethers-adapters': 'ethers',
-    },
-  },
-
-  optimizeDeps: {
-    include: ['ethers'],
-  },
-
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-})
+});
