1c54c7b47182674ccc3ba9aec55574ec48d05965
Jonveil <rifaat.aslam@gmail.com>
2026-02-01 10:50:59 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Setting khas untuk App Blockchain (Thirdweb)
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Ini 'ubat' paling penting untuk elak error "Global not defined"
+    "global": "window",
+  },
 });
