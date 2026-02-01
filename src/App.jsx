82779bc06f97f206417a5631b6f3e1895abf1050
Jonveil <rifaat.aslam@gmail.com>
2026-02-01 13:17:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// trigger fresh build
+// rebuild
+
 import React, { useState } from "react";
 import { ConnectWallet, useAddress, useContract, useNFTBalance } from "@thirdweb-dev/react";
 import Governance from "./Governance.jsx";
