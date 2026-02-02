9f59ee96db833b2b7249b0ebd466bd6c02f5af07
Jonveil <rifaat.aslam@gmail.com>
2026-02-02 10:17:57 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { WorldChain } from "@thirdweb-dev/chains";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Kita tak payah import WorldChain. Kita guna ID dia je terus.
+// Chain ID World Chain = 480
+
+const activeChain = 480; 
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ThirdwebProvider 
-      activeChain={WorldChain} 
-      clientId="b25286cc43a81f0ecab40b732a0d462c"
+      activeChain={activeChain} 
+      clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
     >
       <App />
     </ThirdwebProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
