ee6d7ec15091c3e41636d554ebecc0dedda23b8c
Jonveil <rifaat.aslam@gmail.com>
2026-02-02 08:47:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  // Konfigurasi untuk pastikan variable global wujud
   define: {
+    // Wajib ada untuk Thirdweb berfungsi
     "global": "window",
     "process.env": {},
-  },
-  resolve: {
-    alias: {
-      // 🔥 HELAH PENYELAMAT (The Fix) 🔥
-      // Bila Vercel cari fail-fail 'Safe' ni, kita tipu dia.
-      // Kita halakan dia ke pakej 'ethers' yang memang dah ada.
-      "@safe-global/safe-ethers-adapters": "ethers",
-      "@safe-global/safe-core-sdk": "ethers",
-      "@safe-global/safe-service-client": "ethers"
-    },
   },
 });
