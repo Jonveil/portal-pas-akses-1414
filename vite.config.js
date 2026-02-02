c8e5d1c7bdcc707468ea0bfd2757e80cd731d182
Jonveil <rifaat.aslam@gmail.com>
2026-02-02 08:59:39 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Wajib ada untuk Thirdweb berfungsi
     "global": "window",
     "process.env": {},
+  },
+  build: {
+    rollupOptions: {
+      // 🔥 KUASA VETO:
+      // Senaraikan fail degil ini sebagai 'external'.
+      // Netlify takkan cuba bundle fail ini, jadi build akan LULUS.
+      external: [
+        "@safe-global/safe-ethers-adapters",
+        "@safe-global/safe-core-sdk",
+        "@safe-global/safe-service-client"
+      ],
+    },
   },
 });
