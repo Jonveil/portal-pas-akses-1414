83f61d2416fa09a837a77005dabfe0b50908b0be
Jonveil <rifaat.aslam@gmail.com>
2026-02-06 21:08:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  define: {
-    global: 'window',
-    'process.env': {},
-  },
-
-  resolve: {
-    alias: {
-      process: 'process/browser',
-      stream: 'stream-browserify',
-      util: 'util',
-      buffer: 'buffer',
-      // zlib tak perlu kalau tak guna compression
-    },
-  },
-
-  build: {
-    target: 'es2020',
-    rollupOptions: {
-      external: [
-        /^@safe-global\/.*$/,
-        /^@walletconnect\/.*$/,
-        'crypto',
-        'events',
-      ],
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-})
+});
