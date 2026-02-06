fd0cfc6a4f38811e06a8a46034b12f3cc71a1de3
Jonveil <rifaat.aslam@gmail.com>
2026-02-06 21:54:51 +0800

    Wrap App component with ThirdwebProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { ThirdwebProvider } from "thirdweb/react";
+import { createThirdwebClient } from "thirdweb";
+import { defineChain } from "thirdweb/chains";
+
+const client = createThirdwebClient({
+  clientId: "b25286cc43a81f0ecab40b732a0d462c",
+});
+
+const chain = defineChain(480); // World Chain
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThirdwebProvider client={client} activeChain={chain}>
+      <App />
+    </ThirdwebProvider>
   </React.StrictMode>
 );
