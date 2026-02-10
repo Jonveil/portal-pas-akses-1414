9d7258ff878b28d5c78b878202c1c86e96ea476b
Jonveil <rifaat.aslam@gmail.com>
2026-02-10 16:05:00 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { ThirdwebProvider } from "thirdweb/react";
-import { createThirdwebClient } from "thirdweb";
-import { defineChain } from "thirdweb/chains";
-
-const client = createThirdwebClient({
-  clientId: "b25286cc43a81f0ecab40b732a0d462c",
-});
-
-const chain = defineChain(480); // World Chain
+import { MiniKitProvider } from "@worldcoin/minikit-react";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThirdwebProvider client={client} activeChain={chain}>
+    <MiniKitProvider
+      config={{
+        app_id: "app_7147fcca529b9e4c5181157a356d9378", // ID Mini App kau
+      }}
+    >
       <App />
-    </ThirdwebProvider>
+    </MiniKitProvider>
   </React.StrictMode>
 );
