424b8790ae1de020c47d85d42e9e703ff1e62c65
Jonveil <rifaat.aslam@gmail.com>
2026-02-11 19:37:52 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { MiniKitProvider } from "@worldcoin/minikit-react";
-
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MiniKitProvider
-      config={{
-        app_id: "app_7147fcca529b9e4c5181157a356d9378", // ID Mini App kau
-      }}
-    >
-      <App />
-    </MiniKitProvider>
+    <App />
   </React.StrictMode>
 );
